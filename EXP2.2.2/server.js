c66f6c723cd2963a7278a000ea9c56c988678ec0
suneet0001 <suneet3051@gmail.com>
2026-03-18 21:41:15 +0530

    Update server.js

--- EXP2.2.2/server.js
+++ EXP2.2.2/server.js
@@ -2,19 +2,24 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cors = require("cors")
+const path = require("path")   
 
 dotenv.config()
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
-app.use(express.static("public"))
+
+app.use(express.static(path.join(__dirname, "public")))
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
+.then(()=>console.log("MongoDB Atlas Connected"))
 .catch(err=>console.log(err))
 
 const authRoutes = require("./routes/authRoutes")
+
+// API routes
 app.use("/api", authRoutes)
 
 app.listen(process.env.PORT, ()=>{
